3b56954e22a06e75b37877d3da9f3af794f35d35
dammy10 <adediped@gmail.com>
2026-07-22 16:37:05 +0300

    latest changes now

--- nextjs-dashboard/app/ui/dashboard/sidenav.tsx
+++ nextjs-dashboard/app/ui/dashboard/sidenav.tsx
@@ -7,18 +7,22 @@ import { signOut } from "@/auth";
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="mb-6 flex items-center justify-between rounded-xl bg-blue-500 px-4 py-5 text-white shadow-md md:mb-8">
+        <div className="flex items-center gap-3">
+          <AcmeLogo />
+        </div>
+      </div>
+      <div className="flex grow flex-col justify-between space-y-4 md:space-y-6">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
           action={async () => {
             "use server";
             await signOut({ redirectTo: "/" });
           }}
         >
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:justify-start md:px-4">
             <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
+            <span>Sign Out</span>
           </button>
         </form>
       </div>
